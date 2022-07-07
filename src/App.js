b35c1f84262f01d0ef321bd094b778c9144b2603
Raul Espinoza Muñoz <57574392+raespinoza-pe@users.noreply.github.com>
2022-07-07 00:49:24 -0500

    instalando plantilla web de login, register y forget

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
+import Login from './page/login';
+import Register from './page/register';
+import Forgot from './page/forgot';
 import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login/>}></Route>
+        <Route path="/login" element={<Login/>}></Route>
+        <Route path="/register" element={<Register/>}></Route>
+        <Route path="/forgot" element={<Forgot/>}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/page/forgot.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Forgot  = () => {
+
+
+    return (
+
+        <div>
+
+            <h1>Olvidaste contraseña</h1>
+
+        </div>
+
+    )
+
+
+}
+
+export default Forgot
