a2bc351a172336e596cdbe1320bbfabc50ecd30a
Raul Espinoza Muñoz <57574392+raespinoza-pe@users.noreply.github.com>
2022-10-17 21:07:53 -0500

    home

--- src/page/index.js
+++ src/page/index.js
@@ -1,6 +1,7 @@
 import React from "react"
-
-
+import '../css/style.css'
+import logo1 from '../img/logo1.png'
+import icon from '../img/icon.png'
 
 const Destino = () => {
 
@@ -8,18 +9,18 @@ const Destino = () => {
 
         <>
 
-            <div class="banner">
-                <div class="header">
-                    <div class="container">
-                        <div class="header-main">
-                            <div class="logo wow bounceInLeft" data-wow-delay="0.3s">
-                                <a href="index.html"><img src="images/logo1.png" alt=""/></a>
+            <div className="banner">
+                <div className="header">
+                    <div className="container">
+                        <div className="header-main">
+                            <div className="logo wow bounceInLeft" data-wow-delay="0.3s">
+                                <a href="index.html"><img src= {logo1} alt=""/></a>
                             </div>
-                            <div class="top-navg">
-                                <span class="menu"> <img src="images/icon.png" alt="" /></span>
-                                <nav class="cl-effect-13">
-                                    <ul class="res">
-                                        <li><a class="active" href="index.html">Home</a></li>
+                            <div className="top-navg">
+                                <span className="menu"> <img src= {icon} alt="" /></span>
+                                <nav className="cl-effect-13">
+                                    <ul className="res">
+                                        <li><a className="active" href="index.html">Home</a></li>
                                         <li><a href="about.html">About</a></li>
                                         <li><a href="hotel.html">Hotel</a></li>
                                         <li><a href="shortcodes.html">Shortcodes</a></li>
@@ -29,32 +30,32 @@ const Destino = () => {
                                 </nav>
 
                             </div>
-                            <div class="clearfix"> </div>
+                            <div className="clearfix"> </div>
                         </div>
                     </div>
                 </div>
-                <div class="banner-main">
-                    <div class="slider-bann wow bounceInRight" data-wow-delay="0.3s">
-                        <ul class="rslides" id="slider2">
+                <div className="banner-main">
+                    <div className="slider-bann wow bounceInRight" data-wow-delay="0.3s">
+                        <ul className="rslides" id="slider2">
                             <li>
                                 <h3>Welcome To Our Travel</h3>
                                 <h4>The Best Way To Be Lost</h4>
-                                <div class="ban-btn">
-                                    <a href="single.html" class="hvr-bounce-out">Load More</a>
+                                <div className="ban-btn">
+                                    <a href="single.html" className="hvr-bounce-out">Load More</a>
                                 </div>
                             </li>
                             <li>
                                 <h3>Find Beautiful Place</h3>
                                 <h4>The Best Way To Be Lost</h4>
-                                <div class="ban-btn">
-                                    <a href="single.html" class="hvr-bounce-out">Load More</a>
+                                <div className="ban-btn">
+                                    <a href="single.html" className="hvr-bounce-out">Load More</a>
                                 </div>
                             </li>
                             <li>
                                 <h3>To travel is to live</h3>
                                 <h4>The Best Way To Be Lost</h4>
-                                <div class="ban-btn">
-                                    <a href="single.html" class="hvr-bounce-out">Load More</a>
+                                <div className="ban-btn">
+                                    <a href="single.html" className="hvr-bounce-out">Load More</a>
                                 </div>
                             </li>
                         </ul>
